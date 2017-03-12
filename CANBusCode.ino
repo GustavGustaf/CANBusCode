c48593982d27499ff0e35c7bf0034538951e0c93
Gustave <gustavegustaf@outlook.com>
2017-03-12 14:44:14 -0500

    Changed Code Output
    Code now outputs one fixed-length string for LabView.

--- CANBusCode.ino
+++ CANBusCode.ino
@@ -9,13 +9,26 @@ char msgString[128];                        // Array to store serial string
 #define CAN0_INT 2                              // Set INT to pin 2
 MCP_CAN CAN0(10);                               // Set CS to pin 10
 
+int scaleIt(double value){ //This function will scale all values to an integer between 100 and 999.
+  int scaledValue=(value*899)+100;
+  return scaledValue;
+}
+
+//Initialize variables to 0 since they are sent from the CANBus at different rates.
+double rpm=0;
+double tps=0;
+double mabsp=0;
+double lambda=0;
+double oilTemp=0;
+double frequency2=0;
+double coolantTemp=0;
 
 void setup()
 {
   Serial.begin(9600);
   
   // Initialize MCP2515 running at 16MHz with a baudrate of 250kb/s and the masks and filters disabled.
-  if(CAN0.begin(MCP_STDEXT/*MCP_STDEXT  MCP_ANY*/, CAN_250KBPS, MCP_16MHZ) == CAN_OK)
+  if(CAN0.begin(MCP_STDEXT, CAN_250KBPS, MCP_16MHZ) == CAN_OK) //Changed from MCP_ANY to MCP_STDEXT
     Serial.println("MCP2515 Initialized Successfully!");
   else
     Serial.println("Error Initializing MCP2515...");
@@ -25,66 +38,40 @@ void setup()
   pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
   
   Serial.println("MCP2515 Library Receive Example...");
+
 }
 
 void loop()
 {
   if(!digitalRead(CAN0_INT))                         // If CAN0_INT pin is low, read receive buffer
   {
+    String outString=""; //This will store the fixed-length values to be sent to LabView
+
+    //All readings are turned into a percentage of their max values.
     CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
-//    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
-//    sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len   
-//    Serial.println();
-//    Serial.print(rxId);
-
-//Throttle Position.
-//Coolant temp.
-//Oil temp.
-//Lambda.
-//Map.
-//Frequency2.
-//Rpm.
-    Serial.println();
     if(rxId == 2365583432){ //Extended ID: 0x0CFFF048
-      Serial.print("RPM");
-      double RPM=(rxBuf[1]*256+rxBuf[0]);
-      Serial.print(RPM);
-      Serial.print("Throttle Percentage");
-      double percentage=(rxBuf[3]*256+rxBuf[2])/10.0;
-      Serial.print(percentage);
+      rpm=(rxBuf[1]*256+rxBuf[0])/30000.0; //Rotations per minute
+      tps=((rxBuf[3]*256+rxBuf[2])/10.0)/100; //Throttle position as a percentage
     }
-    Serial.println();
+    else if(rxId == 2365583688){ //Extended ID: 0x0CFFF148
+      mabsp=((rxBuf[3]*256+rxBuf[2])/100.0)/300; //Manifold absolute pressure
+      lambda=((rxBuf[5]*256+rxBuf[4])/100.0)/10; //Oxygen
+    }
+    else if(rxId == 2365583944){ //Extended ID: 0CFFF248
+      oilTemp=((rxBuf[1]*256+rxBuf[0])/1000.0)/5; //Oil temperature from Analog Input #1
+    }
+    else if(rxId == 2365584456){ //Extended ID: 0CFFF448
+      frequency2=((rxBuf[3]*256+rxBuf[2])/10.0)/6000; //Reading from Frequency2
+    }
+    else if(rxId == 2365584712){ //Extended ID: 0x0CFFF548
+      coolantTemp=(((rxBuf[5]*256+rxBuf[4])/10.0)+1000)/2000; //Coolant temperature
+    }
+    else {
+      sprintf(msgString, "rxId: %.8lX not recognized", rxId);
+      Serial.println(msgString);
+    }
 
-      if(rxId == 2365583688){ //Extended ID: 0x0CFFF148
-      Serial.print("MAP");
-      double MAP=(rxBuf[3]*256+rxBuf[2])/100.0;
-      Serial.print(MAP);
-      Serial.print("Lambda");
-      double lambda=(rxBuf[5]*256+rxBuf[4])/100.0;
-      Serial.print(lambda);
-      }
-    Serial.println();
-      if(rxId == 2365583944){ //Extended ID: 0CFFF248
-        Serial.print("OilTemp");
-        double Oiltemp=(rxBuf[1]*256+rxBuf[0])/1000.0;
-        Serial.print(Oiltemp);
-      }
-    Serial.println();
-    
-      if(rxId == 2365584456){ //Extended ID: 0CFFF448
-        Serial.print("Frequency2");
-        double Frequency2=(rxBuf[3]*256+rxBuf[2])/1000.0;
-        Serial.print(Frequency2);
-      }
-      Serial.println();
-      if(rxId == 2365584712){ //Extended ID: 0x0CFFF548
-        Serial.print("CoolantTemp");
-        double coolanttemp=(rxBuf[5]*256+rxBuf[4])/10.0;
-        Serial.print(coolanttemp);
-      }
+    outString=scaleIt(rpm)+scaleIt(tps)+scaleIt(mabsp)+scaleIt(lambda)+scaleIt(oilTemp)+scaleIt(frequency2)+scaleIt(coolantTemp);
+    Serial.println(outString);
   }
 }
-
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
